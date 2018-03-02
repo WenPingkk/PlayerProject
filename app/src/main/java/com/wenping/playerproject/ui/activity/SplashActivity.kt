5097d49bfca7f9eb704c5dd54e496e1d2a3bdba8
kk <wenpingkk@163.com>
2018-03-02 22:51:57 +0800

    004 定义theme style->图片显示全屏

---
+++ app/src/main/java/com/wenping/playerproject/ui/activity/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.wenping.playerproject.ui.activity
+
+import com.wenping.playerproject.R
+import com.wenping.playerproject.base.BaseActivity
+
+/**
+ * Author WenPing
+ * CreateTime 2018/3/2.
+ *
+ */
+class SplashActivity: BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_splash
+    }
+}
