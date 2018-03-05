87bfd913351877dc5bdc2ab88b92850bf769b594
wenping <wenpingkk@163.com>
2018-03-05 11:19:30 +0800

    008 实现toolbar菜单的点击:tipof接口只存在一个方法待实现

--- app/src/main/java/com/wenping/playerproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/wenping/playerproject/ui/activity/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.wenping.playerproject.ui.activity
 
+import android.support.v7.widget.Toolbar
 import com.wenping.playerproject.R
 import com.wenping.playerproject.base.BaseActivity
+import com.wenping.playerproject.util.ToolBarManager
+import org.jetbrains.anko.find
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity() ,ToolBarManager{
+    //惰性加载,toolbar 在用的时候才初始化:by lazy 线程安全哦!!!
+    override val toolbar by lazy {
+        find<Toolbar>(R.id.toolbar)
+    }
     override fun getLayoutId(): Int {
         return R.layout.activity_main
     }
 
+    override fun initData() {
+        super.initData()
+        initMainToolBar()
+    }
 }
---
+++ app/src/main/java/com/wenping/playerproject/util/ToolBarManager.kt
@@ -0,0 +1,55 @@
+package com.wenping.playerproject.util
+
+import android.content.Intent
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.Toast
+import com.wenping.playerproject.R
+import com.wenping.playerproject.ui.activity.SettingActivity
+
+
+/**
+ * @author WenPing
+ * @date 2018/3/5
+ *<p>
+ */
+interface ToolBarManager {
+
+    val toolbar: Toolbar
+
+    /**
+     * 初始化mainAct中的toolbar
+     */
+    fun initMainToolBar() {
+        //SET TITLE
+        toolbar.setTitle("kk影音")
+        //inflate 菜单布局
+        toolbar.inflateMenu(R.menu.menu)
+
+        //kotlin和java之间调用的特性
+        //OnMenuItemClickListener 接口中只有一个未实现的方法;可以把接口省略掉->大括号打开
+        //如果java接口中只有一个未实现的方法,可以省略接口对象,直接复写这个方法
+        toolbar.setOnMenuItemClickListener {
+            //println("item=$it")
+            toolbar.context.startActivity(Intent(toolbar.context, SettingActivity::class.java))
+            true
+        }
+
+        //点击效果
+//        toolbar.setOnMenuItemClickListener(object :Toolbar.OnMenuItemClickListener{
+//            override fun onMenuItemClick(item: MenuItem?): Boolean {
+//
+//                when (item?.itemId) {
+//                    R.id.setting->{
+//                        //Toast.makeText(toolbar.context,"点击了设置按钮",Toast.LENGTH_SHORT).show()
+//                        //跳转到setting页面
+//                        //kotlin 的:: class ;java的::class.java
+//                        toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
+//                    }
+//                }
+//                //true 消费事件
+//                return true
+//            }
+//        })
+    }
+}
